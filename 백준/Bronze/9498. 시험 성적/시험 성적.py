f840d34feb87b05a87b84426b3e433ec88b44535
이정선 <121466466+ljs524@users.noreply.github.com>
2023-01-18 14:02:58 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+Num = int(input())
+if 90 <= Num <= 100:
+    print('A')
+elif 80 <= Num <= 89:
+    print('B')
+elif 70 <= Num <= 79:
+    print('C')
+elif 60 <= Num <= 69:
+    print('D')
+else:
+    print('F')
